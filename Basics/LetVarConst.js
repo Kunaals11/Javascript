2bd5080edbae64f1125e12f22867c05a0239c467
Your Name <kunaals11@gmail.com>
2024-12-02 16:26:06 +0530

    1st

---
+++ Basics/LetVarConst.js
@@ -0,0 +1,17 @@
+const accountId = 123456;
+
+let Name = "Kunaal";
+var password = 321654;
+
+surname = "sharma"
+
+// accountId = 645862  we can not change constants
+// console.log(accountId)
+
+
+Name = "Raghav";
+password = 465341231;
+
+surname = "Kumar";
+
+console.table([Name,password,surname])
